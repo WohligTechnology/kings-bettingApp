9faee38011d9bdcbf575b10900cba43e7d77f8b9
sailirawnang <saili.rawnang@wohlig.com>
2018-08-09 13:16:53 +0530

    All changes

---
+++ www/templates/home/home.js
@@ -0,0 +1,4 @@
+myApp.controller('HomeCtrl', function ($scope, $ionicModal, $timeout) {
+
+
+})
---
+++ www/templates/open-bet/open-bet.js
@@ -0,0 +1,31 @@
+myApp.controller('OpenBetCtrl', function ($scope, $ionicModal, $timeout) {
+
+  //Accordian Code
+  $scope.groups = [];
+  for (var i = 0; i < 10; i++) {
+    $scope.groups[i] = {
+      name: i,
+      items: []
+    };
+    for (var j = 0; j < 3; j++) {
+      $scope.groups[i].items.push(i + '-' + j);
+    }
+  }
+
+  /*
+   * if given group is the selected group, deselect it
+   * else, select the given group
+   */
+  $scope.toggleGroup = function (group) {
+    if ($scope.isGroupShown(group)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = group;
+    }
+  };
+  $scope.isGroupShown = function (group) {
+    return $scope.shownGroup === group;
+  };
+
+
+})
