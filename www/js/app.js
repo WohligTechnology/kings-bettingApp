2e5acd9e34c13e311277aa0fd3663ca9cc42e8d1
sohan1 <shn619@gmail.com>
2018-09-24 12:18:55 +0530

    some ui -fixes

--- www/js/app.js
+++ www/js/app.js
@@ -146,38 +146,11 @@ var myApp = angular.module('starter', ['ionic', 'starter.controllers'])
           }
         }
       })
-      .state('app.browse', {
-        url: '/browse',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/browse.html'
-          }
-        }
-      })
       .state('login', {
         url: '/login',
         templateUrl: 'templates/login/login.html',
         controller: 'LoginCtrl'
-      })
-      .state('app.playlists', {
-        url: '/playlists',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/playlists.html',
-            controller: 'PlaylistsCtrl'
-          }
-        }
-      })
-
-      .state('app.single', {
-        url: '/playlists/:playlistId',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/playlist.html',
-            controller: 'PlaylistCtrl'
-          }
-        }
       });
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/playlists');
+    $urlRouterProvider.otherwise('/login');
   });
