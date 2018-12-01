c67fc4ff140a2c12b317ad1cbd0dd44d5b612538
kothavaleganesh <ganesh.kothavale@wohlig.com>
2018-12-01 16:18:37 +0530

    done

--- www/templates/match-inner/match-inner.js
+++ www/templates/match-inner/match-inner.js
@@ -1,21 +1,5 @@
 myApp.controller('MatchinnerCtrl', function ($scope, $ionicModal, $timeout) {
 
-  //Accordian Code
-  $scope.groups = [];
-  for (var i = 0; i < 2; i++) {
-    $scope.groups[i] = {
-      name: i,
-      items: []
-    };
-    for (var j = 0; j < 2; j++) {
-      $scope.groups[i].items.push(i + '-' + j);
-    }
-  }
-
-  /*
-   * if given group is the selected group, deselect it
-   * else, select the given group
-   */
   $scope.toggleGroup = function (group) {
     if ($scope.isGroupShown(group)) {
       $scope.shownGroup = null;
@@ -31,16 +15,4 @@ myApp.controller('MatchinnerCtrl', function ($scope, $ionicModal, $timeout) {
     $scope.bet = !$scope.bet
   }
 
-  //code for modal
-  $ionicModal.fromTemplateUrl('templates/modal/confirmbet.html', {
-    scope: $scope
-  }).then(function (modal) {
-    $scope.confirmbet = modal;
-  });
-  $scope.confirmbetShow = function () {
-    $scope.confirmbet.show();
-  };
-  $scope.confirmbetHide = function () {
-    $scope.confirmbet.hide();
-  };
 })
