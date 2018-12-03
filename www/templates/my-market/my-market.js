06d722280478645ba77b058e18a1cd32f02286d0
kothavaleganesh <ganesh.kothavale@wohlig.com>
2018-12-03 12:25:21 +0530

    done my markets

--- www/templates/my-market/my-market.js
+++ www/templates/my-market/my-market.js
@@ -1,4 +1,24 @@
-myApp.controller('MymarketCtrl', function ($scope, $ionicModal, $timeout) {
+myApp.controller('MymarketCtrl', function ($scope, $ionicModal, $timeout, Service) {
 
 
+  $scope.getMyMarket = function () {
+    $scope.loadingData = true;
+    var user = $.jStorage.get("userId");
+    Service.apiCallWithData(
+      "category/getMyMarket", {
+        user: user
+      },
+      function (data) {
+        if (data.value) {
+          $scope.myMarkets = data.data;
+          $scope.loadingData = false;
+        } else {
+          $scope.myMarkets = [];
+          $scope.loadingData = false;
+        }
+      }
+    );
+  };
+
+  $scope.getMyMarket();
 })
